b94f333ccd8b2e62de51da4e3471fc525e0c2883
Rajesh Krishnan <rkrishnan@perspectalabs.com>
2019-12-04 01:50:10 -0500

    Use general render when possible, fix idgen

--- source/scratch/scenspec.py
+++ source/scratch/scenspec.py
@@ -51,18 +51,35 @@ def compose(n,d):
 
 # Generic traversal using depth-first search
 def traverse(v,name,depth,style,layout=None):
-  ret = ""
+  ret = ''
   if valid_class_instance(v): 
     ret += v.render(depth,style=style,layout=layout)
     for i in fields(v): 
       x = getattr(v,i)
       if isinstance(x,list):
-        ret += "".join([traverse(j,i,depth+1,style,layout) for j in x])
+        ret += ''.join([traverse(j,i,depth+1,style,layout) for j in x])
       else:
         ret += traverse(x,i,depth+1,style,layout)
   else:
     ret += base.field_render(depth,v,name,style,layout=layout)
   return ret
+
+# Generic rendering of all children that are class instances or list thereof
+def render_children(v,depth,style,layout,exclude=[]): 
+  ret = ''
+  for i in fields(v): 
+    if i in exclude: continue
+    x = getattr(v,i)
+    if isinstance(x,list):
+      ret += ''.join([j.render(depth+1,style,layout) for j in x])
+    elif valid_class_instance(x): 
+      ret += x.render(depth+1,style,layout)
+    else:
+      pass # ignore all non class fields in generic render_children
+  return ret
+
+##############################################
+# IMN scenario generator specifc code below
 
 class IDGen():
   def __init__(self):
@@ -71,67 +88,120 @@ class IDGen():
     self.cid = 0
     self.nm2id = {}
 
-  def get_id(nm,typ):
-    if nm not in nm2id: 
+  def get_id(self,nm,typ):
+    if nm not in self.nm2id: 
       if typ in ['NODE', 'xdhost', 'inthost', 'hub', 'xdgateway']:
-        nm2id[nm] = 'n'+str(self.nid)
+        self.nm2id[nm] = 'n'+str(self.nid)
         self.nid += 1
       elif typ in ['link', 'left', 'right']:
-        nm2id[nm] = 'l'+str(self.lid)
+        self.nm2id[nm] = 'l'+str(self.lid)
         self.lid += 1
       elif typ in ['canvas']:
-        nm2id[nm] = 'c'+str(self.cid)
+        self.nm2id[nm] = 'c'+str(self.cid)
         self.cid += 1
-    return nm2id[nm]
+    return self.nm2id[nm]
 
-# Scenario classes derived from base class
-class scenario(base):  
+class basewid(base):  
+  _idgen = IDGen()  # class member, common for instances of this class and all subclasses
+
+# Scenario classes 
+class scenario(basewid):  
   def render(self,depth,style='imn',layout=None): 
-    if style is 'imn':
-      # handle enclave[]
-      #for e in enclave:
-      #  for x in e.xdhost:
-      #      x.hwconf
-      # handle xdgateway[]
-      # handle xdlink[]
-      # handle additional stuff such as canvas, 
-      return "foostring"
-    else:
-      return super().render(depth,style)
+    return render_children(self,depth,style,layout) if style is 'imn' else super().render(depth,style,layout)
 
-class enclave(base): pass
-class xdhost(base): pass
-class hwconf(base): pass
-class swconf(base): pass
-class nwconf(base): pass
-class interface(base): pass
-class ifpeer(base): pass
-class inthost(base): pass
-class link(base): pass
-class hub(base): pass
-class xdgateway(base): pass
-class xdlink(base): pass
-class left(base): pass
-class right(base): pass
-class egress(base): pass
-class ingress(base): pass
+class enclave(basewid): 
+  def render(self,depth,style='imn',layout=None): 
+    return render_children(self,depth,style,layout) if style is 'imn' else super().render(depth,style,layout)
 
-# Layout classes also derived from base class
-class layout(base): 
+## TODO
+class xdhost(basewid): 
+  def render(self,depth,style='imn',layout=None): 
+    return 'xdhost to be handled\n' if style is 'imn' else super().render(depth,style,layout)
+
+## TODO
+class inthost(basewid): 
+  def render(self,depth,style='imn',layout=None): 
+    return 'inthost to be handled\n' if style is 'imn' else super().render(depth,style,layout)
+
+## TODO
+class hub(basewid): 
+  def render(self,depth,style='imn',layout=None): 
+    tstr = 'hub to be handled, use of get_id: ' + self.hostname + ' ' + basewid._idgen.get_id(self.hostname,'hub')
+    return tstr + '\n' if style is 'imn' else super().render(depth,style,layout)
+
+## TODO
+class xdgateway(basewid): 
+  def render(self,depth,style='imn',layout=None): 
+    return 'xdgateway to be handled\n' if style is 'imn' else super().render(depth,style,layout)
+
+## TODO
+class link(basewid): 
+  def render(self,depth,style='imn',layout=None): 
+    return 'link to be handled\n' if style is 'imn' else super().render(depth,style,layout)
+
+## TODO
+class xdlink(basewid): 
+  def render(self,depth,style='imn',layout=None): 
+    return 'xdlink to be handled\n' if style is 'imn' else super().render(depth,style,layout)
+
+## TODO
+class hwconf(basewid): pass
+## TODO
+class swconf(basewid): pass
+## TODO
+class nwconf(basewid): pass
+## TODO
+class interface(basewid): pass
+## TODO
+class ifpeer(basewid): pass
+## TODO
+class left(basewid): pass
+## TODO
+class right(basewid): pass
+## TODO
+class egress(basewid): pass
+## TODO
+class ingress(basewid): pass
+
+# Layout classes
+## TODO
+class scenlayout(basewid): 
+  def render(self,depth,style='imn',layout=None): 
+    return render_children(self,depth,style,layout,exclude=['nodelayout']) if style is 'imn' else super().render(depth,style,layout)
   def get_node_layout(nod):
     x = [n for n in self.nodelayout if n.hostname == nod]
     if len(x) != 1: raise Exception ('Error getting layout for:' + nod)
     return x[0] 
- 
-class canvas(base): pass
-class option(base): pass
-class optglobal(base): pass
-class session(base): pass
-class nodelayout(base): pass
-class iconcoords(base): pass
-class labelcoords(base): pass
-class annotation(base): pass
-class bbox(base): pass
+
+class option(basewid):
+  def render(self,depth,style='imn',layout=None): 
+    return render_children(self,depth,style,layout) if style is 'imn' else super().render(depth,style,layout)
+
+## TODO
+class nodelayout(basewid): pass
+## TODO
+class iconcoords(basewid): pass
+## TODO
+class labelcoords(basewid): pass
+
+## TODO
+class canvas(basewid):
+  def render(self,depth,style='imn',layout=None): 
+    return 'canvas to be handled\n' if style is 'imn' else super().render(depth,style,layout)
+## TODO
+class optglobal(basewid):
+  def render(self,depth,style='imn',layout=None): 
+    return 'optglobal to be handled\n' if style is 'imn' else super().render(depth,style,layout)
+## TODO
+class session(basewid):
+  def render(self,depth,style='imn',layout=None): 
+    return 'session to be handled\n' if style is 'imn' else super().render(depth,style,layout)
+## TODO
+class annotation(basewid):
+  def render(self,depth,style='imn',layout=None): 
+    return 'annotation to be handled\n' if style is 'imn' else super().render(depth,style,layout)
+## TODO
+class bbox(basewid): pass
 
 if __name__ == '__main__':
   args = get_args()
@@ -139,9 +209,11 @@ if __name__ == '__main__':
   with open(args.layout, 'r') as inf2: layo = json.load(inf2)
 
   scen = compose('scenario',conf)
-  locs = compose('layout',layo)
+  locs = compose('scenlayout',layo)
 
   #print(traverse(scen,'scenario',0,'basic',locs))
-  #print(traverse(locs,'layout',  0,'basic',locs))
+  #print(traverse(locs,'scenlayout',  0,'basic',locs))
 
-  #scen.render(0,'imn',locs)
+  ret = scen.render(0,'imn',locs)
+  ret += locs.render(0,'imn',None)
+  print(ret)
