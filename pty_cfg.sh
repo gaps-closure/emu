2542aebab02337ea0852ba20ac27c6e7536002ec
Tony McAuley <amcauley@perspectalabs.com>
2019-12-04 12:51:27 -0500

    Correct ssh private key

--- pty_cfg.sh
+++ pty_cfg.sh
@@ -5,14 +5,25 @@ if [ "$USER" != "root" ]; then
 else
     USER_INITIALS=$(echo $SESSION_FILENAME | awk -F/ '{print $3}' | cut -c1-2 )
 fi
-DIR_IMGS=$(echo $SESSION_FILENAME | sed 's:emulator.*$:emulator/imgs:')
+#SSHPRIVKEY="/home/amcauley/gaps/top-level/emulator/config/id_rsa"
+SSHPRIVKEY="/home/amcauley/gaps/top-level/emulator/build_qemu_vm/build/id_closure_rsa"
+
+################################################################################################
+# COMMON ENCLAVE DEVICE CONFIGURATION
+################################################################################################
 DEV_PTY_PREFIX="/dev/vcom"
 LOG_FILE_PREFIX="/tmp/socat"
 
+################################################################################################
+# UNIQUE ENCLAVE DEVICE CONFIGURATION
+################################################################################################
 ORANGE_NAME="orange"
 ORANGE_SUBNET_IP=1
+ORANGE_GW_PORT="12345"
+
 PURPLE_NAME="purple"
 PURPLE_SUBNET_IP=2
+PURPLE_GW_PORT="12346"
 
 
 # Main
@@ -20,23 +31,29 @@ echo "Starting socat on $HOSTNAME"
 
 ENCLAVE_NAME=$ORANGE_NAME
 GW_IP="10.0.${ORANGE_SUBNET_IP}.1"
-GW_PORT="12345"
+##TESTIBG
+#GW_IP="10.201.${ORANGE_SUBNET_IP}.1"
+GW_PORT="$ORANGE_GW_PORT"
+
 if [[ $HOSTNAME == *"$PURPLE_NAME"* ]]; then
   ENCLAVE_NAME="$PURPLE_NAME"
   GW_IP="10.0.${PURPLE_SUBNET_IP}.1"
-  GW_PORT="12346"
+  GW_PORT="$PURPLE_GW_PORT"
 fi
+
 DEV_PTY="${DEV_PTY_PREFIX}_${USER_INITIALS}_${ENCLAVE_NAME}"
 LOG="${LOG_FILE_PREFIX}_${USER_INITIALS}_${ENCLAVE_NAME}.log"
+MAN_NIF_IP="10.200.0.1"
+
 
 echo "e=$ENCLAVE_NAME i=$GW_IP p=$GW_PORT d=$DEV_PTY l=$LOG"
-echo "ssh -i /home/amcauley/gaps/top-level/emulator/config/id_rsa closure@10.200.0.1 sudo socat -d -d -lf ${LOG} pty,link=${DEV_PTY},raw,ignoreeof,unlink-close=0,echo=0 tcp:${GW_IP}:${GW_PORT},ignoreeof &"
+echo "ssh -i $SSHPRIVKEY closure@10.200.0.1 sudo socat -d -d -lf ${LOG} pty,link=${DEV_PTY},raw,ignoreeof,unlink-close=0,echo=0 tcp:${GW_IP}:${GW_PORT},ignoreeof &"
 
-ssh -i /home/amcauley/gaps/top-level/emulator/config/id_rsa closure@10.200.0.1 ls
-ssh -i /home/amcauley/gaps/top-level/emulator/config/id_rsa closure@10.200.0.1 \
+ssh -i $SSHPRIVKEY closure@${MAN_NIF_IP} ls
+ssh -i $SSHPRIVKEY closure@${MAN_NIF_IP} \
   sudo socat -d -d -lf ${LOG} \
   pty,link=${DEV_PTY},raw,ignoreeof,unlink-close=0,echo=0 \
   tcp:${GW_IP}:${GW_PORT},ignoreeof &
 sleep 1
-ssh -i /home/amcauley/gaps/top-level/emulator/config/id_rsa closure@10.200.0.1 \
+ssh -i $SSHPRIVKEY closure@${MAN_NIF_IP} \
   sudo chmod 666 ${DEV_PTY}
