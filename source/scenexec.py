6d8f5fbbd65b5eedf162526a61ab7211e759e06d
Michael Kaplan <mkaplan@perspectalabs.com>
2019-12-11 22:45:06 -0500

    vm's start, and checked for ssh

--- source/scenexec.py
+++ source/scenexec.py
@@ -5,6 +5,7 @@ import time
 def execute(scenario, layout, settings, args):
     create_qemu_snapshots(scenario, settings, clean=False)
     start_core_scenario(scenario, settings, args.outfile)
+    check_vm_status(scenario, settings)
 
 def clean_snapshots(settings):
     subprocess.run(['rm', '-rf', settings.snapdir])
@@ -61,9 +62,29 @@ def make_netplan(xdhost, outfile):
 def start_core_scenario(scenario, settings, filename):
     if not os.path.exists(filename):
         raise Exception ("CORE scenario file not found: " + filename)
-    subprocess.Popen(["core-gui", "-s", filename])
-    time.sleep(2) # TODO: check by other means
+    print(f'Starting CORE session (filename={filename})...', end="", flush=True)
+    subprocess.Popen(["core-gui", "-s", filename], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    time.sleep(2) # give CORE a chance to start
     p = subprocess.run([settings.emuroot + '/scripts/core/get_core_session.sh'], stdout=subprocess.PIPE)
-    time.sleep(5) # TODO: verify scenario start is complete
     setattr(scenario, 'core_session_id', int(p.stdout))
-    
+    tries = 0
+    while(tries < settings.core_timeout):
+        f = open(f'/tmp/pycore.{scenario.core_session_id}/state', 'r')
+        if 'RUNTIME' in f.readline():
+            print(f'pycore={scenario.core_session_id}...DONE!', flush=True)
+            f.close()
+            return
+        tries+=1
+        time.sleep(1)
+    raise Exception('ERROR: CORE session failed to start')
+
+def check_vm_status(scenario, settings):
+    for enc in scenario.enclave:
+        for x in enc.xdhost:
+            print(f'Checking QEMU up at {x.hostname}...', end="", flush=True)
+            core_path = f'/tmp/pycore.{scenario.core_session_id}/{x.hostname}'
+            res = subprocess.check_output(['vcmd', '-c', core_path, '--', 'scripts/xdhost/check_vm.sh'], text=True)
+            if 'SUCCESS' not in res:
+                raise Exception (f'Failure to boot/ssh into {x.hostname}: {res}')
+            print('DONE!', flush=True)
+
