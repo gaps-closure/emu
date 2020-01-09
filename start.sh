402fc40d65777bedb7015248b3c822dc13d7463a
Michael Kaplan <mkaplan@perspectalabs.com>
2020-01-08 22:24:03 -0500

    sets user in emu_root

--- start.sh
+++ start.sh
@@ -2,4 +2,7 @@
 SZ=$1
 SCEN=${SZ}enclave
 
+ME=`whoami`
+sed -i -e "s/<user>/${ME}/" config/${SCEN}/${SCEN}_settings.json
+
 python3 source/scenspec.py -s config/${SCEN}/${SCEN}_settings.json -f config/${SCEN}/${SCEN}_config.json -l config/${SCEN}/${SCEN}_imnlayout.json -o ${SCEN}.imn
