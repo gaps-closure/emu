f023c11ca3ec948266a7c434d0b9d74fc59c2c9a
Michael Kaplan <mkaplan@perspectalabs.com>
2020-01-15 03:02:54 +0000

    name change inside

--- scripts/qemu/qemu-example-customize.sh
+++ scripts/qemu/qemu-example-customize.sh
@@ -1,10 +1,10 @@
-./emulator_customize.sh -g /IMAGES/ubuntu-amd64-eoan-qemu.qcow2 \
+./qemu-emulator-customize.sh -g /IMAGES/ubuntu-amd64-eoan-qemu.qcow2 \
                         -k /IMAGES/linux-kernel-amd64-eoan \
                         -a amd64 \
                         -o ubuntu-19.10-amd64-closure-orange-enclave-gw.qcow2 \
                         -n $HOME/gaps/top-level/emulator/config/qemu_config_netplan_core_x86.txt 
                         
-./emulator_customize.sh -g /IMAGES/ubuntu-arm64-eoan-qemu.qcow2 \
+./qemu-emulator-customize.sh -g /IMAGES/ubuntu-arm64-eoan-qemu.qcow2 \
                         -k /IMAGES/linux-kernel-arm64-xenial \
                         -a arm64 \
                         -o ubuntu-19.10-arm64-closure-purple-enclave-gw.qcow2 \
