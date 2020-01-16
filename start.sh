2934ba3c012456a59bd3f0bd5153bddc563e0ba3
Michael Kaplan <mkaplan@perspectalabs.com>
2020-01-16 15:11:25 -0500

    for IDE

--- start.sh
+++ start.sh
@@ -5,4 +5,9 @@ SCEN=${SZ}enclave
 ME=`whoami`
 sed -i -e "s/<user>/${ME}/" config/${SCEN}/${SCEN}_settings.json
 
+clear
+echo "========================== STARTING EMULATOR ============================"
 python3 source/scenspec.py -s config/${SCEN}/${SCEN}_settings.json -f config/${SCEN}/${SCEN}_config.json -l config/${SCEN}/${SCEN}_imnlayout.json -o ${SCEN}.imn
+echo "========================== EMULATOR RUNNING  ============================"
+read -p "Stop CORE and press Enter to terminate."
+
