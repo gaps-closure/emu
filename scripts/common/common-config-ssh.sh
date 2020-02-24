03764561d3944d904241d7c423929fd9a8e540ac
Michael Kaplan <mkaplan@perspectalabs.com>
2020-02-24 11:44:48 -0500

    define vm for easy access to mgmt iface

--- scripts/common/common-config-ssh.sh
+++ scripts/common/common-config-ssh.sh
@@ -5,6 +5,15 @@ KEYDIR="$1"
 mkdir -p /root/.ssh
 echo "Host *" > /root/.ssh/config
 echo "    StrictHostKeyChecking no" >> /root/.ssh/config
+
+echo "Host vm" >> /root/.ssh/config
+echo "    Port 22" >> /root/.ssh/config
+echo "    User closure" >> /root/.ssh/config
+echo "    IdentityFile /root/.ssh/id_closure_rsa" >> /root/.ssh/config
+echo "    StrictHostKeyChecking no" >> /root/.ssh/config
+echo "    HostName 10.200.0.1" >> /root/.ssh/config
+echo "    HostKeyAlias vm.local" >> /root/.ssh/config
+
 chmod 400 /root/.ssh/config
 
 cp ${KEYDIR}/id_closure* /root/.ssh
