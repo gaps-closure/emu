c9533a666a17ab4f502c6c1c7487119b97b799ce
Rajesh Krishnan <rkrishnan@perspectalabs.com>
2020-04-16 11:06:10 -0400

    write if not empty

--- tools/filterproc.py
+++ tools/filterproc.py
@@ -5,5 +5,6 @@ import sys
 if __name__ == "__main__":
   while True:
     c = sys.stdin.read(1)
-    sys.stdout.write(c)
-    sys.stdout.flush()
+    if c:
+      sys.stdout.write(c)
+      sys.stdout.flush()
