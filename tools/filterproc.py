d516c5bdae6e9820e521018906dd2264ab829e45
Rajesh Krishnan <rkrishnan@perspectalabs.com>
2020-04-16 10:56:57 -0400

    invokve python3 with -u; change fiterproc.py to read character at a time

--- tools/filterproc.py
+++ tools/filterproc.py
@@ -3,9 +3,7 @@
 import sys
 
 if __name__ == "__main__":
-    for line in sys.stdin:
-#        line = line.replace('a', 'x')
-        sys.stdout.write(line)
-        sys.stdout.flush()
-#        print ("o1:", type(line), line, end = '')
-#        sys.stderr.write(line)
+  while True:
+    c = sys.stdin.read(1)
+    sys.stdout.write(c)
+    sys.stdout.flush()
